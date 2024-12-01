97b0c9f0acbe32416e1b86e5e3ce2e4aa6fb040b
swtmply <delosreyesjohnallen@gmail.com>
2024-12-01 13:44:56 +0800

    fix: removed export on uploadthing file router

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -7,7 +7,7 @@ import {
 const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
+const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
     image: { maxFileSize: "4MB", maxFileCount: 1 },
